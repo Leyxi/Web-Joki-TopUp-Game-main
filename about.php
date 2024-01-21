03d868aa673c7c68884c84597c5f7603da6a4eee
Aidyl Syaputra <123472399+Leyxi@users.noreply.github.com>
2024-01-21 15:45:09 +0800

    first commit

---
+++ about.php
@@ -0,0 +1,2 @@
+<img src="img/BEKBEKBEK.jpg" width="35%">
+<p>Project WEB dari Aidyl Syaputra </p>
---
+++ footer.php
@@ -0,0 +1,24 @@
+<?php include 'koneksi.php'; ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="footer.css">
+    <title></title>
+</head>
+<body>
+    
+</body>
+</html>
+    <!-- <div id="container_footer"> -->
+        <div class="footer1">
+            <!-- <div class="footer2"> -->
+                <div class="text-footer">
+                    <h1>Top Up & Joki Game</h1>
+                    <p>Copyright 2024</p>
+                    <p>Aidyl Syaputra</p>
+                </div>
+            </div>
+
